d019b9cb44bf7139a47f4ce3e2b002c27a0c7c1f
Bristisul <bristi9801@gmail.com>
2025-12-17 20:28:08 +0600

    Day 4

---
+++ Day 4/Slicing Surgon.py
@@ -0,0 +1,5 @@
+data = [10,20,30,40,50,60]
+
+reverse_data = data[-1:-4:-1]
+
+print(reverse_data)
---
+++ Day 4/Stack Emulator.py
@@ -0,0 +1,8 @@
+data = []
+data.append(1)
+data.append(2)
+data.append(3)
+print(data)
+
+data.pop()
+print(data)
